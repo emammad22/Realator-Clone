bfb7871b0aaea64cf6a05cdd8905b87782fb0df4
emammad22 <mammadoveltun22@gmail.com>
2023-10-18 17:13:14 +0400

    added pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SigIn from "./pages/SigIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 export default function App() {
   return (
-    <h1 className="text-2xl bg-red-400">Hello World</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/sign-in" element={<SigIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
